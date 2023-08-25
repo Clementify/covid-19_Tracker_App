203da861689f1b59ef1626b0af244be70834d8b2
clementify <obiclementina92@gmail.com>
2023-08-25 15:27:57 +0100

    hosting

--- tasks/tracker.js
+++ tasks/tracker.js
@@ -1,44 +1,50 @@
-// Write something
-const fetchapi = `https://disease.sh/v3/covid-19/countries`;
-// Function to fetch COVID-19 data for a selected country
-function getCountryData() {
-    const countrySelect = document.getElementById("country");
-    const selectedCountry = countrySelect.value;
-    fetchapi(`https://disease.sh/v3/covid-19/countries`)
-        .then((response) => response.json())
-        .then((data) => {
-            // Calculate total cases, deaths, and recovered
-            const totalCases = data[data.length - 1].Confirmed;
-            const totalDeaths = data[data.length - 1].Deaths;
-            const totalRecovered = data[data.length - 1].Recovered;
-            // Update the UI with the data
-            document.getElementById("totalCases").textContent = totalCases;
-            document.getElementById("totalDeaths").textContent = totalDeaths;
-            document.getElementById("totalRecovered").textContent =
-                totalRecovered;
-        })
-        .catch((error) => {
-            console.error("Error fetching data:", error);
-        });
-}
- getCountryData()
-// Fetch a list of countries when the page loads
-fetchapi("https://disease.sh/v3/covid-19/countries")
-    .then((response) => response.json())
-    .then((countries) => {
-        const countrySelect = document.getElementById("country");
-        countries.forEach((country) => {
-            const option = document.createElement("option");
-            option.value = country.Slug;
-            option.textContent = country.Country;
-            countrySelect.appendChild(option);
-        });
-    })
-    .catch((error) => {
-        console.error("Error fetching country data:", error);
-    });
-
 
 
 // create function to display covid data and paginate
 
+
+const countryCards = document.querySelector('.country-cards');
+const countryInfo = document.querySelector('.country-info');
+
+// Function to fetch COVID-19 data and create country cards
+async function fetchDataAndCreateCards() {
+    try {
+        const response = await fetch('https://disease.sh/v3/covid-19/countries');
+        const data = await response.json();
+
+        // Loop through the data and create country cards
+        data.forEach(country => {
+            const card = document.createElement('div');
+            card.classList.add('country-card');
+            card.innerHTML = `
+                <img src="${country.countryInfo.flag}" alt="${country.country}" />
+                <h3>${country.country}</h3>
+                <p>Continent: ${country.continent}</p>
+            `;
+
+            // Add click event to show country information
+            card.addEventListener('click', () => {
+                displayCountryInfo(country);
+            });
+
+            countryCards.appendChild(card);
+        });
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+// Function to display detailed information for a selected country
+function displayCountryInfo(country) {
+    countryInfo.innerHTML = `
+        <h2>${country.country}</h2>
+        <p>Population: ${country.population}</p>
+        <p>Reported Cases Today: ${country.todayCases}</p>
+        <p>Total Reported Cases: ${country.cases}</p>
+        <p>Deaths: ${country.deaths}</p>
+        <p>Recovered Cases: ${country.recovered}</p>
+    `;
+}
+
+// Initialize the application
+fetchDataAndCreateCards();
